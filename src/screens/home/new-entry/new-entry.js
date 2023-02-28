5a8d6a5666280d451747d80ad03eb9da91ba7d02
Ross Clark <rosscee1998@gmail.com>
2023-02-28 18:19:22 +0000

    set up initial react hook form for log creation

--- src/screens/home/new-entry/new-entry.js
+++ src/screens/home/new-entry/new-entry.js
@@ -1,5 +1,81 @@
-import { Text } from "react-native";
+import { Button, Text, TextInput, View } from "react-native";
+import { useForm, Controller } from "react-hook-form";
 
 export default function NewEntry() {
-  return <Text>New Entry screen</Text>;
+  const { control, handleSubmit } = useForm({
+    defaultValues: {
+      venueName: "",
+      category: "",
+      dateTime: "",
+      location: "",
+      comments: "",
+    },
+  });
+
+  return (
+    <>
+      <Text>New Entry screen</Text>
+      <View>
+        <Text>Venue name</Text>
+        <Controller
+          name="venueName"
+          control={control}
+          render={({ field: { value, onChange } }) => (
+            <TextInput
+              value={value}
+              onChangeText={(value) => onChange(value)}
+            />
+          )}
+        />
+        <Text>Category</Text>
+        <Controller
+          name="category"
+          control={control}
+          render={({ field: { value, onChange } }) => (
+            <TextInput
+              value={value}
+              onChangeText={(value) => onChange(value)}
+            />
+          )}
+        />
+        <Text>Date/time</Text>
+        <Controller
+          name="dateTime"
+          control={control}
+          render={({ field: { value, onChange } }) => (
+            <TextInput
+              value={value}
+              onChangeText={(value) => onChange(value)}
+            />
+          )}
+        />
+        <Text>Location</Text>
+        <Controller
+          name="location"
+          control={control}
+          render={({ field: { value, onChange } }) => (
+            <TextInput
+              value={value}
+              onChangeText={(value) => onChange(value)}
+            />
+          )}
+        />
+        <Text>Comments</Text>
+        <Controller
+          name="comments"
+          control={control}
+          render={({ field: { value, onChange } }) => (
+            <TextInput
+              value={value}
+              onChangeText={(value) => onChange(value)}
+            />
+          )}
+        />
+        <Button
+          title="Create"
+          onPress={handleSubmit((data) => console.log(data))}
+        />
+      </View>
+    </>
+  );
 }
